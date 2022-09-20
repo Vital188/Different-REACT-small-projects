84d96a02f55103ee57be32f6b5d23723fdd71377
Vitalij <vitalij.cernysiov@gmail.com>
2022-09-20 16:47:17 +0300

    Continue

--- src/010/components/Repeat/Range.jsx
+++ src/010/components/Repeat/Range.jsx
@@ -1,31 +1,76 @@
 import { useState } from "react";
 
 function Range() {
+  const [dog, setDog] = useState([]);
+  const [size, setSize] = useState();
+  const [col, setCol] = useState();
 
-    const [dog, setDog] = useState([])
+  const save = () => {
+    setDog((s) => [
+      ...s,
+      {
+        color: col,
+        size: size + "px",
+      },
+    ]);
+    setCol();
+    setSize();
+  };
 
-    const change = () => {
-        setDog(s => [...s, {
-            
-
-        }])
-    }
-
+  const pokyt = () => {
+    setCol("black");
+    setSize(100);
+  };
 
   return (
     <div>
-      <h1>range</h1>
-      <input type="range" id="volume" min="10" max="200" />
-      <label for="volume">Size</label>
-      <input
-        type="color"
-        id="cowbell"
-      />
-      <label for="cowbell">Cowbell</label>
-      {
-        dog.map((tt, i) => <div key={i} className="small"></div>)
-      }
-      <button onClick={change}>POKYTIS</button>
+      Uzsaugoti kvadratai
+      {dog.map((tt, i) => (
+        <div
+          key={i}
+          style={{
+            backgroundColor: tt.color,
+            width: tt.size,
+            height: tt.size,
+          }}
+          className="small"
+        ></div>
+      ))}
+      <div>nupiestas kvadratas
+       {size && col && <div
+          style={{
+            width: size + "px",
+            height: size + "px",
+            backgroundColor: col,
+          }}
+        ></div>} 
+        <div 
+        style={{
+          width: size + "px",
+          height: size + "px",
+          backgroundColor: 'white',
+        }}
+      ></div>
+        <input
+          type="range"
+          id="volume"
+          min="10"
+          max="200"
+          onChange={(e) => setSize(e.target.value)}
+        ></input>
+        <label for="volume">Size</label>
+      </div>
+      <div>
+        <input
+          type="color"
+          id="cowbell"
+          value="#001122"
+          onChange={(e) => setCol(e.target.value)}
+        />
+        <label for="cowbell">Cowbell</label>
+      </div>
+      <button onClick={save}>Save</button>
+      <button onClick={pokyt}>Pokytis</button>
     </div>
   );
 }
